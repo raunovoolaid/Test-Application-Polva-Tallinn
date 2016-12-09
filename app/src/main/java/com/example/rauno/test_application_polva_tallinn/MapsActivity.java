dd3d23592d4b976a07aad1b4c91ab84e8f848100
raunovoolaid <rauno.voolaid@windowslive.com>
2016-12-09 14:44:56 +0200

    Route is now being drawn between Tallinn and Põlva.

--- app/src/main/java/com/example/rauno/test_application_polva_tallinn/MapsActivity.java
+++ app/src/main/java/com/example/rauno/test_application_polva_tallinn/MapsActivity.java
@@ -1,8 +1,17 @@
 package com.example.rauno.test_application_polva_tallinn;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.akexorcist.googledirection.DirectionCallback;
+import com.akexorcist.googledirection.GoogleDirection;
+import com.akexorcist.googledirection.constant.TransportMode;
+import com.akexorcist.googledirection.model.Direction;
+import com.akexorcist.googledirection.model.Leg;
+import com.akexorcist.googledirection.model.Route;
+import com.akexorcist.googledirection.util.DirectionConverter;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,6 +21,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.facebook.FacebookSdk;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -52,7 +63,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(polva).title("Marker in Põlva"));
         mMap.addMarker(new MarkerOptions().position(tallinn).title("Marker in Tallinn"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(polva));
-        mMap.addPolyline(polylineOptions);
+        // Kasutan siin Android-GoogleDirectionLibrary APT't (http://www.akexorcist.com/2015/12/google-direction-library-for-android-en.html)
+        GoogleDirection.withServerKey("AIzaSyBdzWk1XCQaKC4lnb9ars-ldnGJ4N6P0nQ")
+                .from(tallinn)
+                .to(polva)
+                .transportMode(TransportMode.DRIVING)
+                .execute(new DirectionCallback() {
+                    @Override
+                    public void onDirectionSuccess(Direction direction, String rawBody) {
+                        String status = direction.getStatus();
+                        System.out.println("ÕNNESTUS" + " " + status);
+                        Route route = direction.getRouteList().get(0);
+                        Leg leg = route.getLegList().get(0);
+                        ArrayList<LatLng> directionPositionList = leg.getDirectionPoint();
+                        PolylineOptions polylineOptions = DirectionConverter.createPolyline(getApplicationContext(), directionPositionList, 5, Color.RED);
+                        mMap.addPolyline(polylineOptions);
+                        
+                    }
+
+                    @Override
+                    public void onDirectionFailure(Throwable t) {
+                        System.out.println("Error!");
+                    }
+                });
+
 
     }
 }
